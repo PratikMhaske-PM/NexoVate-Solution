76c00aba8e579345177803803f1cf980e38d955e
Pratik Mhaske <pratikmhaske851@gmail.com>
2025-04-26 21:25:42 +0530

    fix: added emailjs-com as external module in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 ADD THIS LINE
+  base: '/',
+  build: {
+    rollupOptions: {
+      external: ['emailjs-com'], // Add emailjs-com as external
+    },
+  },
 })
