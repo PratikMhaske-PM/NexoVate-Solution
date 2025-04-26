fcd1f8b6d86600336e63f6c46298e158b858ac3e
Pratik Mhaske <pratikmhaske851@gmail.com>
2025-04-26 13:38:29 +0530

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NexoVate-Solution/', // your GitHub repo name
+  build: {
+    rollupOptions: {
+      external: ['emailjs-com'],  // Tell Vite to externalize emailjs-com if it's an external module
+    }
+  }
 })
