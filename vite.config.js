65473c395256c77c2d4741765b8f365f2b29c4ee
Pratik Mhaske <pratikmhaske851@gmail.com>
2025-04-26 21:08:12 +0530

    Added full frontend code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NexoVate-Solution/', // your GitHub repo name
-  build: {
-    rollupOptions: {
-      external: ['emailjs-com'],  // Tell Vite to externalize emailjs-com if it's an external module
-    }
-  }
 })
